41a885dbb6c7f79521e3ef8d1e66b8d82ca2f459
shohagnawaz <snawazdigital@gmail.com>
2022-12-24 18:19:20 +0600

    cart

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -6,20 +6,25 @@ import Product from '../Product/Product';
 const Shop = () => {
 
     const first10 = fakeData.slice(0, 10);
-    const [products, setProducts] = useState(first10)   
+    const [products, setProducts] = useState(first10);
+    const handleAddProduct = (product) => {
+        console.log("adding", product);
+    }
 
     return (
         <div className='shop-container'>
             <div className='product-container'>
                 {
-                products.map(product => <Product product = {product}></Product>)
+                    products.map(product => <Product
+                        handleAddProduct={handleAddProduct}
+                        product={product}></Product>)
                 }
             </div>
 
             <div className='cart-container'>
                 <h1>This is cart</h1>
             </div>
-            
+
         </div>
     );
 };
